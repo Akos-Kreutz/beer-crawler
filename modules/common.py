aae97cbc4544fbe58891fe993f5957161adc954f
Ákos Kreutz <akoskreutz@gmail.com>
2023-11-11 16:23:39 +0100

    Version 2.6

--- modules/common.py
+++ modules/common.py
@@ -9,7 +9,7 @@ import argparse
 LANG = None
 SCRIPT_FOLDER = os.path.dirname(os.path.abspath(sys.argv[0]))
 DAY_TIMESTAMP = datetime.now().strftime("%d-%m-%Y")
-VERSION="2.5"
+VERSION="2.6"
 ARGS = None
 class NotAvailable:
     text = "N/A"
