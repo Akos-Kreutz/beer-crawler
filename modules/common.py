f9594ad3249e4a5b7b5774f63bdad97718368cbb
Ákos Kreutz <akoskreutz@gmail.com>
2023-11-13 10:56:57 +0100

    Version 2.8

--- modules/common.py
+++ modules/common.py
@@ -9,7 +9,7 @@ import argparse
 LANG = None
 SCRIPT_FOLDER = os.path.dirname(os.path.abspath(sys.argv[0]))
 DAY_TIMESTAMP = datetime.now().strftime("%d-%m-%Y")
-VERSION="2.7"
+VERSION="2.8"
 ARGS = None
 class NotAvailable:
     """Class used for convenient error handling."""
